aafc041eb7b5bb21cd71251468a2ccb695432380
Behrooz-H <b.hosseini4u@gmail.com>
2025-05-13 08:06:12 -0700

    give away message to the community

--- my_code/2. Reversed Linkedlist Add Two Numbers.py
+++ my_code/2. Reversed Linkedlist Add Two Numbers.py
@@ -4,6 +4,8 @@ You are given two non-empty linked lists representing two non-negative integers.
 Add the two numbers and return the sum as a linked list.
  This can be done once again
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Give away to the community
 """
 from typing import Optional
 
